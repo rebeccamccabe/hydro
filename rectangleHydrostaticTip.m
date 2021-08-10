64a13ae0ba5703eae1a80e98e26491df3380d8b7
Rebecca McCabe <rmccabe@mit.edu>
2021-08-10 18:18:51 -0400

    got graphs yay

--- rectangleHydrostaticTip.m
+++ rectangleHydrostaticTip.m
@@ -1,14 +1,22 @@
+clear;close all;clc
+
+%% input parameters
 L = 5;
 H = 3;
 h0 = 1;
+CG_x_offset_ratio = 0.20;
+
+%% calculations
 
 % origin: bottom right corner of rectangle
-CG_x = -L/2;
+% x positive is left
+% y positive is up
+CG_x = L/2 - CG_x_offset_ratio*L;
 CG_y = H/2;
 
 if h0 < H/2
     shape_case = 1;
-    theta_c = atand(2 * h0 / L);
+    theta_c1 = atand(2 * h0 / L);
     theta_c2 = atand(H^2 / (2 * L * h0));
 elseif h0 > H/2
     shape_case = 2;
@@ -17,48 +25,84 @@ else
 end
 
 thetas = 0:5:90;
+tipping = logical(size(thetas));
+angles = zeros(size(thetas));
 for i=1:length(thetas)
     theta = thetas(i);
+    [CB_x, CB_y] = get_centroid(theta, theta_c1, theta_c2, L, H, h0, shape_case);
+    [tipping(i), angles(i)] = see_if_tipping(theta, CB_x, CB_y, CG_x, CG_y);
+end
+
+figure
+plot(thetas, tipping, thetas, angles)
+xlabel('Degrees')
+ylabel('Tipping?')
+%improvePlot
+
+%% helper functions
+function [tipping, angle] = see_if_tipping(theta, CB_x, CB_y, CG_x, CG_y)
+    tipping = false;
     
-    if theta < theta_c_1
+    delta_x = CG_x - CB_x;
+    delta_y = CG_y - CB_y;
+    alpha = atand(delta_x / delta_y);
+    angle = alpha - theta;
+    
+    if angle < 0
+        tipping = true;
+    end
+    if any([delta_x delta_y]<0)
+        %tipping = NaN;
+    end
+end
+
+function [C_x, C_y] = get_centroid(theta, theta_c1, theta_c2, L, H, h0, shape_case)
+    if theta < theta_c1
         % A
-        Area = .5 * L *
-    elseif theta == theta_c_1
-        % B
+        h2 = h0 + L/2*tand(theta);
+        h1 = h0 - L/2*tand(theta);
+        [C_x, C_y] = centroid_trapezoid(L, h2, h1);
+%     elseif theta == theta_c1
+%         % B
+%         switch shape_case
+%             case 1
+%                 % formula
+%             case 2
+%                 % formula
+%         end
+    elseif theta <= theta_c2
+        % B, C, D
         switch shape_case
             case 1
-                % formula
+                l1 = sqrt(2*L*h0/tand(theta));
+                h1 = sqrt(2*L*h0*tand(theta));
+                [C_x, C_y] = centroid_triangle( l1, h1 );
             case 2
                 % formula
         end
-    elseif theta < theta_c_2
-        % C
-        switch shape_case
-            case 1
-                % formula
-            case 2
-                % formula
-        end
-    elseif theta == theta_c_2
-        % D
-        switch shape_case
-            case 1
-                % formula
-            case 2
-                % formula
-        end
+%     elseif theta == theta_c2
+%         % D
+%         switch shape_case
+%             case 1
+%                 % formula
+%             case 2
+%                 % formula
+%         end
     elseif theta <= 90
         % E
         switch shape_case
             case 1
-                % formula
+                l2 = L*h0/H - H/(2*tand(theta));
+                l1 = L*h0/H + H/(2*tand(theta));
+                [C_x_tmp, C_y_tmp] = centroid_trapezoid(H, l2, l1);
+                C_x = C_y_tmp;
+                C_y = H - C_x_tmp;
             case 2
                 % formula
         end
     else
         error('theta > 90 not supported')
     end
-    
 end
 
 function [C_x, C_y] = centroid_triangle(base, height)
